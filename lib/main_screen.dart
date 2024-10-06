ce6253a90529314d1da44c50e74881a0a2734169
HeRku <herrikurniawan211@gmail.com>
2024-10-07 02:46:17 +0800

    first-commit

---
+++ lib/main_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MainScreen());
+
+class MainScreen extends StatelessWidget{
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    throw UnimplementedError();
+  }
+}
